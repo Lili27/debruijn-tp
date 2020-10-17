43502c1e3ac3793732d08342f28dcbe6c224d24d
Hollier Laetitia <laetitia-hollier@outlook.fr>
2020-10-17 21:44:37 +0200

    commit script debruijn.py

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -6,7 +6,7 @@
 #    (at your option) any later version.
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #    GNU General Public License for more details.
 #    A copy of the GNU General Public License is available at
 #    http://www.gnu.org/licenses/gpl-3.0.html
@@ -16,21 +16,23 @@
 import argparse
 import os
 import sys
+#import statistics
+#from random import randint
+#import random
+#from operator import itemgetter
 import networkx as nx
-import matplotlib
-from operator import itemgetter
-import random
-random.seed(9001)
-from random import randint
-import statistics
+import matplotlib.pyplot as plt
+#random.seed(9001)
 
-__author__ = "Your Name"
+
+
+__author__ = "HOLLIER Laëtitia"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["HOLLIER Laëtitia"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "HOLLIER Laëtitia"
+__email__ = "laetitia-hollier@outlook.fr"
 __status__ = "Developpement"
 
 def isfile(path):
@@ -64,10 +66,74 @@ def get_arguments():
                         help="Output contigs in fasta file")
     return parser.parse_args()
 
+##########################################################
+########### 1. Création du graphe de de Bruijn ###########
+##########################################################
 
-#==============================================================
+def read_fastq(nom):
+    """
+    La fonction read_fastq prend en argument
+    nom : un fichier fastq (str)
+    renvoit les reads de ce fichier
+    sous forme d'un argument de sequences
+    """
+    with open(nom) as filin:
+        for line in enumerate(filin):
+            yield next(filin)[:-1]
+            next(filin)
+            next(filin)
+
+def cut_kmer(seq, k):
+    """
+    La fonction cut_mer prend 2 arguments:
+    seq : une sequence sous forme d'une chaine de caractères (str)
+    k : la taille du k-mer (integer)
+    renvoit les k-mers uniques présents dans les reads
+    """
+    for i in range(len(seq)-k+1):
+        yield seq[i:i+k]
+
+
+def build_kmer_dict(nom,k):
+    """
+    La fonction build_kmer_dict prend en argument
+    nom : un fichier fastq (str)
+    k : la taille du k-mer (integer)
+    renvoit un dictionnaire comportant le k-mer (str) et
+    la valeur du nombre d'occurence de ce k-mer (int)
+    """
+    dict_kmer = {}
+    for i in read_fastq(nom):
+        for j in cut_kmer(i,k):
+            if j not in dict_kmer:
+                dict_kmer[j] = 1
+            else:
+                dict_kmer[j] += 1
+    return dict_kmer
+
+
+def build_graph(dico):
+    """
+    La fonction build_graph prend en argument
+    dico : dictionnaire possédant
+    les k-mers et leur nombre d'occurence
+    et renvoit un arbre orienté et pondéré
+    """
+    arbre_kmer = nx.DiGraph()
+    for mot in dico:
+        arbre_kmer.add_edge(mot[0:len(mot)-1],
+            mot[1:],weight = dico[mot])
+    print(nx.info(arbre_kmer))
+    nx.draw(arbre_kmer)
+    plt.show()
+    plt.savefig("Figure1: 1er Graphe Orienté.png")
+    return arbre_kmer
+
+
+
+#================================================
 # Main program
-#==============================================================
+#================================================
 def main():
     """
     Main program function
@@ -76,4 +142,5 @@ def main():
     args = get_arguments()
 
 if __name__ == '__main__':
-    main()
+    kmer = build_kmer_dict(sys.argv[2],int(sys.argv[4]))
+    graphe = build_graph(kmer)
