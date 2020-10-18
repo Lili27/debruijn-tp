e9157e2565412246b617eed7c033d9ff1c8cefe5
Hollier Laetitia <laetitia-hollier@outlook.fr>
2020-10-18 13:11:07 +0200

    commit de la 2e partie du code

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -19,7 +19,7 @@ import sys
 #import statistics
 #from random import randint
 #import random
-#from operator import itemgetter
+from operator import itemgetter
 import networkx as nx
 import matplotlib.pyplot as plt
 #random.seed(9001)
@@ -66,13 +66,14 @@ def get_arguments():
                         help="Output contigs in fasta file")
     return parser.parse_args()
 
+
 ##########################################################
 ########### 1. Création du graphe de de Bruijn ###########
 ##########################################################
 
 def read_fastq(nom):
     """
-    La fonction read_fastq prend en argument
+    La fonction read_fastq prend en entrée
     nom : un fichier fastq (str)
     renvoit les reads de ce fichier
     sous forme d'un argument de sequences
@@ -85,7 +86,7 @@ def read_fastq(nom):
 
 def cut_kmer(seq, k):
     """
-    La fonction cut_mer prend 2 arguments:
+    La fonction cut_mer prend 2 entrée:
     seq : une sequence sous forme d'une chaine de caractères (str)
     k : la taille du k-mer (integer)
     renvoit les k-mers uniques présents dans les reads
@@ -96,7 +97,7 @@ def cut_kmer(seq, k):
 
 def build_kmer_dict(nom,k):
     """
-    La fonction build_kmer_dict prend en argument
+    La fonction build_kmer_dict prend en entrée
     nom : un fichier fastq (str)
     k : la taille du k-mer (integer)
     renvoit un dictionnaire comportant le k-mer (str) et
@@ -114,25 +115,99 @@ def build_kmer_dict(nom,k):
 
 def build_graph(dico):
     """
-    La fonction build_graph prend en argument
-    dico : dictionnaire possédant
-    les k-mers et leur nombre d'occurence
-    et renvoit un arbre orienté et pondéré
+    La fonction build_graph prend en entrée
+    dico : dictionnaire de k-mers (str) et leur nombre d'occurence (int)
+    renvoit un arbre orienté et pondéré
     """
     arbre_kmer = nx.DiGraph()
     for mot in dico:
         arbre_kmer.add_edge(mot[0:len(mot)-1],
             mot[1:],weight = dico[mot])
     print(nx.info(arbre_kmer))
-    nx.draw(arbre_kmer)
+    options = {'node_color': "red", "node_size":500}
+    nx.draw(arbre_kmer,with_labels=True, font_weight='bold', **options)
     plt.show()
-    plt.savefig("Figure1: 1er Graphe Orienté.png")
+    plt.savefig("Figure1: 1er Graphe Orienté de Bruijn.png")
     return arbre_kmer
+
+
+##########################################################
+########### 2. Parcours du graphe de de Bruijn ###########
+##########################################################
+
+
+def get_starting_nodes(arbre):
+    """
+    La fonction get_starting_nodes prend en entrée
+    arbre: un graphe
+    renvoit une liste de noeuds d'entrée (str ou int)
+    """
+    nodes_in = []
+    for node in arbre.nodes:
+        if len(list(arbre.predecessors(node))) == 0:
+            nodes_in.append(node)
+    return nodes_in
+
+
+def get_sink_nodes(arbre):
+    """
+    La fonction get_sink_nodes prend en entrée
+    arbre graphe
+    renvoit une liste de noeuds de sortie (str ou int)
+    """
+    nodes_out = []
+    for node in arbre.nodes:
+        if len(list(arbre.successors(node))) == 0:
+            nodes_out.append(node)
+    return nodes_out
+
+
+def fill(text, width=80):
+    """
+    Split text with a line return to respect fasta format
+    """
+    return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
+
+
+def get_contigs(arbre, entree, sortie):
+    """
+    La fonction get_sink_nodes prend en entrée
+    arbre: graphe
+    entree: une liste de noeurds d'entree (str)
+    sortie: une liste de noeurds de sortie (str)
+    renvoit une liste de tuple avec le contig (str) et sa taille (int)
+    """
+    contigs = []
+    for debut in entree:
+        for fin in sortie:
+            if list(nx.all_simple_paths(arbre, debut, sortie)) != []:
+                path = nx.shortest_path(arbre,debut,fin)
+                seq =""
+                for i in range(len(path)-1):
+                    seq += path[i][-1]
+                contigs.append((seq, len(seq)))
+    return contigs
+
+
+
+def save_contigs(contig,fichier):
+    """
+    La fonction save_contigs prend en entrée
+    une liste de tuple et un nom de fichier de
+    sortie et renvoit un fichier fasta
+    """
+    with open(fichier, "w") as filout:
+        for i,seq in enumerate(contig):
+            filout.write(">contig_{} len={}\n".format(i+1,seq[1]))
+            filout.write(fill(seq[0] + "\n"))
+            filout.write("\n")
+        return filout
+
 
 
 
 #================================================
-# Main program
+#================ Main program ==================
 #================================================
 def main():
     """
@@ -144,3 +219,7 @@ def main():
 if __name__ == '__main__':
     kmer = build_kmer_dict(sys.argv[2],int(sys.argv[4]))
     graphe = build_graph(kmer)
+    entry = get_starting_nodes(graphe)
+    ending = get_sink_nodes(graphe)
+    sequences = get_contigs(graphe,entry,ending)
+    save_contigs(sequences, "path_contig.txt")
